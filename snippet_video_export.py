22ae8142d01dab13efbfb857905a5794ff91e73f
Claromes <claromes@hey.com>
2022-06-09 12:33:48 -0300

    add snippet exp video

---
+++ snippet_video_export.py
@@ -0,0 +1,19 @@
+#source: https://abav.lugaralgum.com/material-aulas/Processing-Python/exportar_animacoes.html
+
+import __main__
+add_library('VideoExport')
+
+file_name = __main__.__file__.split(".", 1)
+
+def setup():
+    global video_export
+
+    video_export = VideoExport(this, , file_name[0] + ".mp4")
+    video_export.startMovie()
+
+def draw():
+    if frameCount < 600:
+        video_export.saveFrame()
+    else:
+        video_export.endMovie()
+        exit()
